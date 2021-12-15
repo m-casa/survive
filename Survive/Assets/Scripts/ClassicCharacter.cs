5285e577100e6ffa19807ab28162c30916400d94
Marc Casanova <marc.casanova@yahoo.com>
2021-12-14 22:17:10 -0600

    Added a survivor with animations; Improved feel

--- Survive/Assets/Scripts/ClassicCharacter.cs
+++ Survive/Assets/Scripts/ClassicCharacter.cs
@@ -1,9 +1,28 @@
+using Animancer;
 using ECM2.Characters;
 using UnityEngine;
 
 public class ClassicCharacter : Character
 {
     #region EDITOR EXPOSED FIELDS
+
+    [Header("Speed Multipliers")]
+    [Tooltip("The speed multiplier while Character is walking forward.")]
+    [SerializeField]
+    private float _forwardSpeedMultiplier;
+
+    [Tooltip("The speed multiplier while Character is walking backward.")]
+    [SerializeField]
+    private float _backwardSpeedMultiplier;
+
+    [Tooltip("The speed multiplier while Character is walking sideways.")]
+    [SerializeField]
+    private float _strafeSpeedMultiplier;
+
+    [Header("Animation Objects")]
+    [Tooltip("The animancer component.")]
+    [SerializeField]
+    private AnimancerComponent animancer;
 
     #endregion
 
@@ -13,9 +32,48 @@ public class ClassicCharacter : Character
 
     #region PROPERTIES
 
+    /// <summary>
+    /// The speed multiplier while Character is walking forward.
+    /// </summary>
+
+    public float forwardSpeedMultiplier
+    {
+        get => _forwardSpeedMultiplier;
+        set => _forwardSpeedMultiplier = Mathf.Max(0.0f, value);
+    }
+
+    /// <summary>
+    /// The speed multiplier while Character is walking backwards.
+    /// </summary>
+
+    public float backwardSpeedMultiplier
+    {
+        get => _backwardSpeedMultiplier;
+        set => _backwardSpeedMultiplier = Mathf.Max(0.0f, value);
+    }
+
+    /// <summary>
+    /// The speed multiplier while Character is strafing.
+    /// </summary>
+
+    public float strafeSpeedMultiplier
+    {
+        get => _strafeSpeedMultiplier;
+        set => _strafeSpeedMultiplier = Mathf.Max(0.0f, value);
+    }
+
     #endregion
 
     #region METHODS
+
+    /// <summary>
+    /// The maximum speed for current movement mode, factoring walking movement direction.
+    /// </summary>
+
+    public override float GetMaxSpeed()
+    {
+        return base.GetMaxSpeed() * GetSpeedMultiplier();
+    }
 
     /// <summary>
     /// Overrides SetupPlayerInput method.
@@ -63,7 +121,7 @@ public class ClassicCharacter : Character
         Vector3 movementDirection = Vector3.zero;
         Vector3 rotationDirection = Vector3.zero;
 
-        movementDirection += GetForwardVector() * movementInput.y;
+        movementDirection += Vector3.Normalize(GetForwardVector() * movementInput.y);
         rotationDirection += Vector3.Normalize(Vector3.right * movementInput.x);
 
         SetMovementDirection(movementDirection);
@@ -83,6 +141,169 @@ public class ClassicCharacter : Character
     }
 
     /// <summary>
+    /// Overrides Animate.
+    /// Update the animation state of the character.
+    /// </summary>
+
+    protected override void Animate()
+    {
+        ChangeMovementAnimation();
+    }
+
+    /// <summary>
+    /// Overrides OnReset.
+    /// Resets speed multipliers.
+    /// </summary>
+
+    protected override void OnReset()
+    {
+        // Character defaults
+
+        base.OnReset();
+
+        // Speed multiplier defaults
+
+        forwardSpeedMultiplier = 1.0f;
+        backwardSpeedMultiplier = 0.5f;
+        strafeSpeedMultiplier = 0.75f;
+
+        SetRotationMode(RotationMode.None);
+    }
+
+    /// <summary>
+    /// Overrides OnOnValidate.
+    /// Validates speed multipliers.
+    /// </summary>
+
+    protected override void OnOnValidate()
+    {
+        // Validates Character fields
+
+        base.OnOnValidate();
+
+        // Validate speed multipliers
+
+        forwardSpeedMultiplier = _forwardSpeedMultiplier;
+        backwardSpeedMultiplier = _backwardSpeedMultiplier;
+        strafeSpeedMultiplier = _strafeSpeedMultiplier;
+    }
+
+    /// <summary>
+    /// The current speed multiplier based on movement direction,
+    /// eg: walking forward, walking backwards or strafing side to side.
+    /// </summary>
+
+    protected virtual float GetSpeedMultiplier()
+    {
+        // Compute planar move direction
+
+        Vector3 up = GetUpVector();
+        Vector3 planarMoveDirection = Vector3.ProjectOnPlane(GetMovementDirection(), up);
+
+        // Compute actual walk speed factoring movement direction
+
+        Vector3 characterForward = Vector3.ProjectOnPlane(GetForwardVector(), up);
+
+        float dot = Vector3.Dot(planarMoveDirection.normalized, characterForward.normalized);
+
+        float speedMultiplier = dot >= 0.0f
+            ? Mathf.Lerp(strafeSpeedMultiplier, forwardSpeedMultiplier, dot)
+            : Mathf.Lerp(strafeSpeedMultiplier, backwardSpeedMultiplier, -dot);
+
+        return speedMultiplier;
+    }
+
+    /// <summary>
+    /// Updates the character's movement animation.
+    /// </summary>
+
+    private void ChangeMovementAnimation()
+    {
+        Vector2 movementInput = GetMovementInput();
+
+        if (movementInput != Vector2.zero)
+        {
+            // We're moving by direction
+            ChangeDirectionalAnimation(movementInput);
+        }
+        else
+        {
+            // We're not moving, so use an idle animation
+            ChangeIdleAnimation();
+        }
+    }
+
+    /// <summary>
+    /// Updates the character's directional animation.
+    /// </summary>
+
+    private void ChangeDirectionalAnimation(Vector2 movementInput)
+    {
+        // Moving forward
+        if (movementInput.y > 0f )//&& (movementInput.x > -0.5f && movementInput.x < 0.5f))
+        {
+            if (IsSprinting())
+            {
+                animancer.TryPlay("Run", 0.25f);
+            }
+            else
+            {
+                animancer.TryPlay("Walk", 0.25f);
+            }
+        }
+
+        // Moving backwards
+        else if (movementInput.y < 0f)//&& (movementInput.x > -0.5f && movementInput.x < 0.5f))
+        {
+            animancer.TryPlay("Walk Backwards", 0.25f);
+        }
+
+        // Turning left
+        else if (movementInput.x < 0)
+        {
+            animancer.TryPlay("Turn Left", 0.25f);
+        }
+
+        // Turning right
+        else if (movementInput.x > 0f)
+        {
+            animancer.TryPlay("Turn Right", 0.25f);
+        }
+    }
+
+    /// <summary>
+    /// Updates the character's idle animation.
+    /// </summary>
+
+    private void ChangeIdleAnimation()
+    {
+        animancer.TryPlay("Idle", 0.25f);
+    }
+
+    /// <summary>
+    /// Set the player's rotation rate based on the their input.
+    /// </summary>
+
+    private void SetRotationRate(Vector2 movementInput)
+    {
+        if (movementInput.y != 0f)
+        {
+            if (IsSprinting())
+            {
+                rotationRate = 180f;
+            }
+            else
+            {
+                rotationRate = 90f;
+            }
+        }
+        else
+        {
+            rotationRate = 130f;
+        }
+    }
+
+    /// <summary>
     /// Only allow the player to sprint forward
     /// </summary>
 
@@ -95,26 +316,6 @@ public class ClassicCharacter : Character
         else if (movementInput.y <= 0f)
         {
             StopSprinting();
-        }
-    }
-
-    /// <summary>
-    /// Set the player's rotation rate based on the their input.
-    /// </summary>
-
-    private void SetRotationRate(Vector2 movementInput)
-    {
-        if (movementInput.y != 0f && IsSprinting())
-        {
-            rotationRate = 165f;
-        }
-        else if (movementInput.y != 0f)
-        {
-            rotationRate = 95f;
-        }
-        else
-        {
-            rotationRate = 130f;
         }
     }
 
